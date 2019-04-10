9322a6371486249f4c3f35e361e2bf46a5ca4086
Tina Ostrander <tostrander@greenriver.edu>
2019-04-10 16:46:36 -0400

    Add default view

--- index.php
+++ index.php
@@ -21,7 +21,10 @@ $f3->set('DEBUG', 3);
 
 //Define a default route
 $f3->route('GET /', function() {
-   echo '<h1>Hola!</h1>';
+
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/home2.html');
 });
 
 //Run Fat-Free
